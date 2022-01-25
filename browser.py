079d5d170b6823abb0cf34162e6a20a74dcb3dcf
Artur Brytkowski <artur.bryt@gmail.com>
2022-01-25 23:21:49 +0100

    Minor code fixes.

--- browser.py
+++ browser.py
@@ -4,7 +4,6 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 # Importing standard packages
 import os
-from pathlib import Path
 
 DRIVER_CLOSED_MESSAGE = 'Unable to evaluate script: disconnected: not connected to DevTools\n'
 
