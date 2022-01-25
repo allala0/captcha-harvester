6af16c57f2f00f441c33d891048ac84ff1763d6e
Artur Brytkowski <artur.bryt@gmail.com>
2022-01-25 23:23:07 +0100

    Changed sitekey source from string to dynamically downloaded from website.

--- example.py
+++ example.py
@@ -2,12 +2,9 @@ from harvester_manager import HarvesterManger
 from harvester import Harvester
 
 url = 'https://www.google.com/recaptcha/api2/demo'
-sitekey = '6Le-wvkSAAAAAPBMRTvw0Q4Muexq9bi0DJwx_mJ-'
+sitekey = Harvester.get_sitekey(url)
 
 harvester_manager = HarvesterManger(response_callback=lambda x: print(x['response']))
 harvester_manager.add_harvester(Harvester(url, sitekey))
 harvester_manager.start_harvesters()
 harvester_manager.main_loop()
-
-
-
